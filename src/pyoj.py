c33239eb6a6e18e47b15180ef522d6ee13a89563
Younghun Ju <yhju83@gmail.com>
2012-08-29 17:56:52 +0900

    Updated to fit in latest changes on ros.

--- src/pyoj.py
+++ src/pyoj.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import roslib
-roslib.load_manifest('rospy')
-roslib.load_manifest('joy')
+roslib.load_manifest('pyoj')
 import rospy
-from joy.msg import Joy
+from sensor_msgs.msg import Joy
 import SocketServer
 import math
 
@@ -15,24 +14,24 @@ class handler(SocketServer.DatagramRequestHandler):
         print "Client %s said ``%s''" % (self.client_address[0], newmsg)
         self.wfile.write(self.server.oldmsg)
         self.server.oldmsg = newmsg
-	args = newmsg.split(',')
-	x = -float(args[2])
-	y = float(args[3])
-	z = float(args[4])
-	d = math.sin(math.radians(5))
-
-	if abs(x) < d:
-	  x = 0
-	else:
-	  x -= x/math.fabs(x)*d
-
-	if abs(z) < d:
-	  z = 0
-	else:
-	  z -= z/math.fabs(z)*d
-
-	k = 1 / math.cos(math.radians(60))
-        pub.publish(Joy([1.5*k*z, 0.0, 0.0, k*x], [1] ))
+        args = newmsg.split(',')
+        x = -float(args[2])
+        y = float(args[3])
+        z = float(args[4])
+        d = math.sin(math.radians(5))
+        
+        if abs(x) < d:
+          x = 0
+        else:
+          x -= x/math.fabs(x)*d
+        
+        if abs(z) < d:
+          z = 0
+        else:
+          z -= z/math.fabs(z)*d
+        
+        k = 1 / math.cos(math.radians(60))
+        pub.publish(Joy(axes=[1.5*k*z, 0.0, 0.0, k*x], buttons=[1] ))
 
 pub = rospy.Publisher('joy', Joy)
 rospy.init_node('pyoj', anonymous=True)
